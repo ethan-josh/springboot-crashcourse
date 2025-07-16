5f1bf42e740453be20bab88f5da5d8b77a4b0f37
ethan-josh <ethanjoshua.camingao@gcash.com>
2025-07-16 14:13:59 +0800

    feat: challenge 2 done

---
+++ challenge-lab2/src/main/java/com/example/demo/challenge_lab2/ChallengeLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo.challenge_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLab2Application.class, args);
+	}
+
+}
---
+++ challenge-lab2/src/test/java/com/example/demo/challenge_lab2/ChallengeLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.challenge_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
